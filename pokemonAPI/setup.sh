e348422d3cfd717443c3995b9220feed7fc1e9d0
Alexandre Mitsuru Kaihara <39930655+mdewinged@users.noreply.github.com>
2021-10-27 18:27:54 -0300

    Finished script

--- pokemonAPI/setup.sh
+++ pokemonAPI/setup.sh
@@ -6,4 +6,7 @@ sudo apt-get install python3.8
 sudo apt-get install python3-pip
 sudo python3 -m pip install --upgrade pip
 sudo pip install flask
-
+sudo apt-get install git
+git clone https://github.com/mdewinged/tac-nuvem.git
+cd tac-nuvem/pokemonAPI/setup.sh
+sudo python tac-nuvem/pokemonAPI/main.py
