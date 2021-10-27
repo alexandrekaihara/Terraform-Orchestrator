fb366985c1e4038c49547a31a8692f0d0e7f0b85
Alexandre Mitsuru Kaihara <39930655+mdewinged@users.noreply.github.com>
2021-10-27 18:16:04 -0300

    Creating virtual machina config file

---
+++ pokemonAPI/setup.py
@@ -0,0 +1,9 @@
+#! /bin/bash
+
+sudo apt-get update
+sudo apt-get upgrade
+sudo apt-get install python3.8
+sudo apt-get install python3-pip
+sudo python3 -m pip install --upgrade pip
+sudo pip install flask
+
