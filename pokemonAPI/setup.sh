d28444afeb5d5087c223afd0c9e7890ee17ad14d
mdewinged <alexandreamk1@gmail.com>
2021-11-19 22:44:19 -0300

    .
    .

--- pokemonAPI/setup.sh
+++ pokemonAPI/setup.sh
@@ -1,12 +1,9 @@
-#! /bin/bash
-
+#!/bin/bash
 sudo apt-get update
-sudo apt-get upgrade
-sudo apt-get install python3.8
-sudo apt-get install python3-pip
+sudo apt-get install python3 -y
+sudo apt-get install python3-pip -y
 sudo python3 -m pip install --upgrade pip
-sudo pip install flask
-sudo apt-get install git
+sudo pip3 install flask
+sudo apt-get install git -y 
 git clone https://github.com/mdewinged/tac-nuvem.git
-cd tac-nuvem/pokemonAPI/setup.sh
-sudo python tac-nuvem/pokemonAPI/main.py
+sudo (crontab -l 2>/dev/null; echo \"@reboot sudo python3 /home/testadmin/tac-nuvem/pokemonAPI/main.py\") | crontab -
